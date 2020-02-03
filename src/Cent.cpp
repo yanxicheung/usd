d300d6318d9ee51fba12b5a71102ba9f7818927f
yanxicheung <yanxicheung@foxmail.com>
2020-02-03 20:12:57 +0800

    fix Cent.format2

--- src/Cent.cpp
+++ src/Cent.cpp
@@ -1,5 +1,51 @@
 #include "Cent.h"
 #include "BaseMacro.h"
+#include <iostream>
+#include <utility>
+#include <list>
+#include "Dollar.h"
+
+using namespace std;
+namespace
+{
+    string makeFormat(WORD32 amount)
+    {
+        return std::to_string(amount)+"￠";
+    }
+
+    using Item = pair<WORD16,WORD16>;
+
+    list<Item> getItems(WORD32 restAmount)
+    {
+       const WORD16 centValue[] = {50,25,10,5,1};
+       list<Item> items;
+       for(int i = 0;i<sizeof(centValue)/sizeof(centValue[0]);++i)
+       {
+           WORD16 valueNum = restAmount/centValue[i];
+           if(restAmount/centValue[i] == 0)
+           {
+               continue;
+           }
+           items.push_back(make_pair(valueNum,centValue[i]));
+           restAmount %= centValue[i];
+       }
+       return items;
+    }
+
+    string combine(const list<Item> items)
+    {
+       string str;
+       for(auto &item:items)
+       {
+           if(not str.empty())
+           {
+               str += ",";
+           }
+           str += std::to_string(item.first) + "*" + makeFormat(item.second);
+       }
+       return str;
+    }
+}
 
 Cent::Cent(WORD32 amount):amount(amount)
 {
@@ -12,10 +58,33 @@ WORD32 Cent::toBaseAmount() const
 
 std::string Cent::format1()
 {
-    return std::to_string(amount)+"￠";
+    return makeFormat(amount);
 }
 
 std::string Cent::format2()
 {
-    return "1*$10,1*25￠,1*5￠";
+    string result;
+    string dolar = Dollar(amount/100).format2();
+    string cent = combine(getItems(amount%100));
+
+    if(not dolar.empty())
+    {
+        if(not cent.empty())
+        {
+            result = dolar + "," + cent;
+        }
+        else
+        {
+            result = dolar;
+        }
+    }
+    else
+    {
+        if(not cent.empty())
+        {
+            result = cent;
+        }
+    }
+
+    return result;
 }
--- test/FormatTest.cpp
+++ test/FormatTest.cpp
@@ -24,16 +24,15 @@ TEST_F(FormatTest,test_dollar_format2)
     ASSERT_TRUE(Dollar(532).format2() == "5*$100,1*$20,1*$10,1*$2");
     ASSERT_TRUE(Dollar(23).format2() == "1*$20,1*$2,1*$1");
     ASSERT_TRUE(Dollar(123).format2() == "1*$100,1*$20,1*$2,1*$1");
+    ASSERT_TRUE(Dollar(0).format2() == "");
+    ASSERT_TRUE(Dollar(103).format2() == "1*$100,1*$2,1*$1");
 }
 
-///*
-// *
-// *目前流行的纸币面值包括：$1，$2，$5，$10，$20，$50，$100；
-//  硬币面值包括：1￠，5￠，10￠，25￠，50￠，$1。
-// *1030￠=> 1*$10，125￠，15￠
-// * */
 TEST_F(FormatTest,test_cent_format2)
 {
     ASSERT_TRUE(Cent(1030).format2() == "1*$10,1*25￠,1*5￠");
+    ASSERT_TRUE(Cent(10300).format2() == "1*$100,1*$2,1*$1");
+    ASSERT_TRUE(Cent(56).format2() == "1*50￠,1*5￠,1*1￠");
+    ASSERT_TRUE(Cent(91).format2() == "1*50￠,1*25￠,1*10￠,1*5￠,1*1￠");
 }
 
